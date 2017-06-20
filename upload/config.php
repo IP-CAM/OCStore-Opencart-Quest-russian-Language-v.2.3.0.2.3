4f7c4c55518a2f5aa19cd4b9f266d312a7b0ed8c
kolyn236 <n.dychkov@digitalwand.ru>
2017-06-20 07:00:20 +0400

    upload application files

---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/OpenCartQuest/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/OpenCartQuest/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/OpenCartQuest/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/OpenCartQuest/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/OpenCartQuest/upload/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/OpenCartQuest/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/OpenCartQuest/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/OpenCartQuest/upload/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/OpenCartQuest/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/OpenCartQuest/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/OpenCartQuest/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/OpenCartQuest/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/OpenCartQuest/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
